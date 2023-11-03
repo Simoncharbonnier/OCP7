640126d008aebdb1896618076d7dff9d224f0f1f
Simoncharbonnier <simoncharbonnier@orange.fr>
2023-11-03 22:13:56 +0100

    feat: delete user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\UserRepository;
 use App\Entity\User;
 
@@ -19,5 +20,14 @@ class UserController extends AbstractController
 
         $jsonUsers = $serializer->serialize($users, 'json', ['groups' => 'getUsers']);
         return new JsonResponse($jsonUsers, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+
+    #[Route('/users/{id}', name: 'deleteUser', methods: ['DELETE'])]
+    public function deleteUser(User $user, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($user);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }

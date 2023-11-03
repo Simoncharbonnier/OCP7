aca944a0d6491c99d86dcb5cb34e6a81f252eac2
Simoncharbonnier <simoncharbonnier@orange.fr>
2023-11-03 22:47:50 +0100

    feat: create and update user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,11 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\UserRepository;
 use App\Entity\User;
+use App\Repository\ClientRepository;
 
 class UserController extends AbstractController
 {
@@ -20,6 +23,36 @@ class UserController extends AbstractController
 
         $jsonUsers = $serializer->serialize($users, 'json', ['groups' => 'getUsers']);
         return new JsonResponse($jsonUsers, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+
+    #[Route('/users', name:'createUser', methods: ['POST'])]
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, ClientRepository $clientRepository): JsonResponse
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $content = $request->toArray();
+        $clientId = $content['client_id'] ?? -1;
+        $user->setClient($clientRepository->find($clientId));
+
+        $em->persist($user);
+        $em->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ['accept' => 'json'], true);
+    }
+
+    #[Route('/users/{id}', name:'updateUser', methods:['PUT'])]
+    public function updateUser(Request $request, SerializerInterface $serializer, User $currentUser, EntityManagerInterface $em, ClientRepository $clientRepository): JsonResponse
+    {
+        $updatedUser = $serializer->deserialize($request->getContent(),
+            User::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $currentUser]);
+
+        $em->persist($updatedUser);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
     #[Route('/users/{id}', name: 'deleteUser', methods: ['DELETE'])]

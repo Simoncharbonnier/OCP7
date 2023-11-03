c3fffa2bff69a2962869d0cf129635f04a691bea
Simoncharbonnier <simoncharbonnier@orange.fr>
2023-11-03 21:32:33 +0100

    feat: get product by id

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\ProductRepository;
 use Symfony\Component\Serializer\SerializerInterface;
+use App\Repository\ProductRepository;
+use App\Entity\Product;
 
 class ProductController extends AbstractController
 {
@@ -17,7 +18,12 @@ class ProductController extends AbstractController
         $products = $productRepository->findAll();
 
         $jsonProducts = $serializer->serialize($products, 'json');
-
         return new JsonResponse($jsonProducts, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/products/{id}', name: 'detailProduct', methods: ['GET'])]
+    public function getProductById(Product $product, SerializerInterface $serializer): JsonResponse {
+        $jsonProduct = $serializer->serialize($product, 'json');
+        return new JsonResponse($jsonProduct, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 }
